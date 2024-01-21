bdcfb2eca6fd26c97e32448bc0cd3b572f0d8351
Abdullah Nazir <124812955+AbdullahNazir0@users.noreply.github.com>
2024-01-21 23:41:48 +0500

    Add files via upload

---
+++ HomeTask_1/Calculator.h
@@ -0,0 +1,22 @@
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+int Subtract(int a, int b)
+{
+    return a - b;
+}
+int Product(int a, int b)
+{
+    return a * b;
+}
+int Divide(int a, int b)
+{
+    return a / b;
+}
+
+#endif
